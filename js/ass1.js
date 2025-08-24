56139bcb414d141fbdfb99d31dd4861f343a07ba
nancy <maryjency22@gmail.com>
2025-08-25 00:57:47 +0530

    uploaded successfully

---
+++ js/ass1.js
@@ -0,0 +1,63 @@
+/**
+var userAge=12;
+var userName="Nancy";
+console.log("Name:" +userName);
+console.log("Age: " +userAge);
+**/
+/** var num1=10;
+var num2=5;
+console.log(num1+num2);
+console.log(num1-num2);
+console.log(num1*num2);
+console.log(num1/num2);
+*/
+
+
+/** 
+ var a=12;
+console.log(typeof a);
+var b="Nancy"
+console.log(typeof b);
+var c=false;
+console.log(typeof c);
+var d=null;
+console.log(typeof d);
+var e;
+console.log(typeof e);
+
+*/
+
+/**
+ function greetUser(name){
+    return ("hi " + name + " have a nice day");
+}
+var username="nancy"
+var msg=greetUser(username);
+console.log(msg);
+
+ */
+
+/**
+ var temperature=40;
+if(temperature>30){
+    console.log("Temperature is very hot");
+}else{
+    console.log("Temperature is moderate");
+}
+*/
+
+/**
+ for(let i=1;i<=5;i++){
+    console.log(i);
+}
+ */
+
+console.log(5=="5");
+console.log(0==false);
+console.log(""==0);
+
+console.log(5==="5");
+console.log(0===false);
+console.log(""===0);
+console.log(5==5);
+
